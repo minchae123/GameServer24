434712eb3e420f7a1df668b4c7e3c92341ec6bb4
minchae123 <08minchae17@gmail.com>
2024-03-11 12:50:28 +0900

    simple chat

---
+++ Socket/Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		using (Socket clinetSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+		{
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("26.8.160.191"), 20000);
+			clinetSocket.Connect(endPoint);
+
+			while (true)
+			{
+				string str = Console.ReadLine();
+				if (str == "exit")
+					return;
+
+				byte[] buffer = Encoding.UTF8.GetBytes(str);
+				clinetSocket.Send(buffer);
+
+				byte[] rbuffer = new byte[256];
+				int byteRead = clinetSocket.Receive(rbuffer);
+
+				if (byteRead < 1)
+				{
+					Console.WriteLine("서버 연결 종료");
+					return;
+				}
+
+				str = Encoding.UTF8.GetString(rbuffer);
+				Console.WriteLine("받음 : " + str);
+			}
+		}
+	}
+}
---
+++ Socket/Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		using(Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+		{
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("26.8.160.191"), 20000);
+
+			serverSocket.Bind(endPoint);
+
+			serverSocket.Listen(20);
+
+			using(Socket clinetSocket = serverSocket.Accept())
+			{
+				Console.WriteLine(clinetSocket.RemoteEndPoint);
+
+				while(true)
+				{
+					byte[] buffer = new byte[256];
+					int totalByte = clinetSocket.Receive(buffer);
+
+					if (totalByte < 1)
+					{
+						Console.WriteLine("클라이언트의 연결 종료");
+						return;
+					}
+
+					string str = Encoding.UTF8.GetString(buffer);
+					Console.WriteLine(str);
+
+					clinetSocket.Send(buffer);
+				}
+			}
+		}
+	}
+}
